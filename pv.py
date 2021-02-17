611149f1ea364c85cda588a0e32a33bb3951e1f5
Francisco Lopez <franlopbri@gmail.com>
2021-02-16 20:12:34 -0600

    se implementa la insercion de datos

--- pv.py
+++ pv.py
@@ -2,9 +2,12 @@ import click
 
 from clients import commands as clients_commands
 
+CLIENTS__TABLE = '.clients.csv'
+
 @click.group()
 @click.pass_context
 def cli(ctx):
     ctx.obj = {}
+    ctx.obj['clients_table'] = CLIENTS__TABLE
 
 cli.add_command(clients_commands.all)
